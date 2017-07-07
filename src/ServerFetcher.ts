52d5a6618bee6b60973c36bee00841280b68ce03
heyitsleo <coderleo42+leocodes@gmail.com>
2017-07-07 13:32:47 -0400

    update lastUpdated

--- src/ServerFetcher.ts
+++ src/ServerFetcher.ts
@@ -70,6 +70,8 @@ export function fetchServers() {
                                     ...response
                                 }).catch(console.error);
                             }
+
+                            connector.update(db, 'info', { ...result }, { lastUpdated: Date.now() }).catch(console.error);
                         });
                     });
                 }
